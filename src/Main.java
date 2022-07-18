181707042f2f8fefd94b47d41c6b2d4bec551f4f
linleckkyalsin <linleckkyalsin@gmail.com>
2022-07-18 17:00:38 +0630

    fix:import java.time.LocalDate;

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
 
-        final String[] DAYS= new String[]{"月","火","水","木","金","土", "日"};
-
+        final String[] DAYS= new String[]{"月", "火", "水", "木", "金", "土", "日"};
         List<User> users=List.of(
                 new User("佐藤美咲", LocalDate.of(1990, 1, 1)),
                 new User("鈴木太郎", LocalDate.of(1991, 2, 2)),
@@ -16,14 +15,13 @@ public class Main {
 
 //      すべてのユーザーを表示する
         System.out.println("【すべてのユーザーを表示する】");
-        users.forEach(user -> System.out.printf("名前: %s, 生年月日: %s\n",user.getName(),user.getBirthdate()));
+        users.forEach(user -> System.out.printf("名前: %s, 生年月日: %s\n", user.getName(), user.getBirthdate()));
 //      すべてのユーザーを表示する。ただし生年月日はyyyy/MM/dd(E)形式で出力する
         System.out.println("【すべてのユーザーを表示する。ただし生年月日はyyyy/MM/dd(E)形式で出力する】");
-        users.forEach(user -> System.out.printf("名前: %s, 生年月日: %s年%s月%s日(%s)\n",user.getName(),user.getBirthdate().getYear(),user.getBirthdate().getMonthValue(),user.getBirthdate().getDayOfMonth(),DAYS[user.getBirthdate().getDayOfWeek().getValue()-1] ));
+        users.forEach(user -> System.out.printf("名前: %s, 生年月日: %s\n", user.getName(), user.getBirthdate().format(DateTimeFormatter.ofPattern("yyyy年MM月dd日(E)")) ));
 //      名前が鈴木で始まる人のみを表示する
         System.out.println("【名前が鈴木で始まる人のみを表示する】");
-        Stream<User> filterList = users.stream().filter(user -> user.getName().startsWith("鈴木"));
-        filterList.forEach(user -> System.out.printf("%s\n",user.getName()));
+        users.stream().filter(user -> user.getName().startsWith("鈴木")).forEach(user -> System.out.printf("%s\n", user.getName()));
 
     }
 }
